fafb2b2fcd176636c0d8b5d30f8b2f25d4a6e0f8
topgradesabroad <vijayshekbkn@gmail.com>
2025-02-03 16:20:31 +0530

    Files and Folders

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}', // Adjust the paths according to your project structure
+    './pages/**/*.{js,ts,jsx,tsx}',  // Next.js pages
+    './components/**/*.{js,ts,jsx,tsx}',  // Next.js components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
